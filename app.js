1a34e1c70deefa4e1a26cf81d462ef051c70f63e
jjggu97 <jjggu@naver.com>
2024-01-21 19:09:56 +0900

    1/21 error

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
-function addTask() {
-    var taskInput = document.getElementById('taskInput');
-    var taskList = document.getElementById('taskList');
-  
-    if (taskInput.value.trim() !== '') {
-      var taskItem = document.createElement('li');
-      taskItem.appendChild(document.createTextNode(taskInput.value));
-      
-      var deleteButton = document.createElement('button');
-      deleteButton.appendChild(document.createTextNode('Delete'));
-      deleteButton.onclick = function() {
-        taskList.removeChild(taskItem);
-      };
-      
-      taskItem.appendChild(deleteButton);
-      taskList.appendChild(taskItem);
-      
-      taskInput.value = '';
-    }
+const themeSwitch = document.getElementById('switch');
+const body = document.body;
 
-    function changeColor(color){
-      document.body.style.backgroundcolor = color;
-    }
-  }
-  
+themeSwitch.addEventListener('change', () => {
+    body.classList.toggle('dark-mode');
+});
